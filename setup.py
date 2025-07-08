6b3f096a74e04548d7cc73437c9a94e8221c46ed
jhell1717 <jhellewell1722@gmail.com>
2025-07-08 21:28:12 +0100

    added utils package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='vae',
     version='0.0.0',
-    packages=find_packages(include=["vae", "data"]),  # match your folders
+    packages=find_packages(include=["vae", "data", "utils"]),  # match your folders
     install_requires=[
         "torch",
     ],
